11df7d469a372b0a2a7b91ceb1e7886b6a6d781a
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-01-04 20:20:06 +0100

    :bug: fix get_user
    If GET['id'] is not set then it will be read from SESSION

--- modules/user/get_user.php
+++ modules/user/get_user.php
@@ -1,6 +1,9 @@
 <?php
 
-$user_id = $_GET['id'];
+session_start();
+
+if (!isset($_GET['id'])) $user_id = $_SESSION['user_id'];
+else $user_id = $_GET['id'];
 $query = require($_SERVER['DOCUMENT_ROOT'].'/modules/db/query.php');
 $user = $query("SELECT * FROM user WHERE id=$user_id;")->fetch_assoc();
 
