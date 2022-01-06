1f6602a67f6ed2ab7615918959ba06450019bc10
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-01-06 06:15:22 +0100

    :recycle: refactor: use switch for including js files

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,10 @@
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <?php if (basename($_SERVER['PHP_SELF']) === 'login.php'): ?>
-  <script src="/assets/js/login.js"></script>
-  <?php endif; ?>
-  <?php if (basename($_SERVER['PHP_SELF']) === 'signup.php'): ?>
-  <script src="/assets/js/signup.js"></script>
-  <?php endif; ?>
-  <?php if (basename($_SERVER['PHP_SELF']) === 'profile.php'): ?>
-  <script src="/assets/js/profile.js"></script>
-  <?php endif; ?>
-  <?php if (basename($_SERVER['PHP_SELF']) === 'change_password.php'): ?>
-  <script src="/assets/js/change_password.js"></script>
-  <?php endif; ?>
+  <?php switch (basename($_SERVER['PHP_SELF'])):
+  ?><?php case 'login.php': ?><script src="/assets/js/login.js"></script><?php break;?>
+    <?php case 'signup.php': ?><script src="/assets/js/signup.js"></script><?php break; ?>
+    <?php case 'profile.php': ?><script src="/assets/js/profile.js"></script><?php break; ?>
+    <?php case 'change_password.php': ?><script src="/assets/js/change_password.js"></script><?php break; ?>
+  <?php endswitch; ?>
 </body>
 </html>
