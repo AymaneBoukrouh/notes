e17f6aad756df5103f96ff52dd30fe82e91f9844
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-01-12 14:55:28 +0100

    :bug: bug: fix host

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,7 @@
   </div>
   <?php $host = (isset($_SERVER['HTTPS_PROTOCOL']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]"; ?>
   <script>
-    const host = '<?= $host ?>';
-    alert(host);
+    const host = '<?= $host ?>'; 
   </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <?php switch (basename($_SERVER['PHP_SELF'])):
